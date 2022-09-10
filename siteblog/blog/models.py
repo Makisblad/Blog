70015e3ec02152fb639ad2df703d27f2086af3a4
Makarova Anastasia <korzhak.an@gmail.com>
2022-09-10 19:27:22 +0300

    Models was created

--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Category (models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self): #служебный метод для вывода title объектов
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+class Tag (models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, verbose_name='Url', unique=True)#уникальное
+
+    def __str__(self): #служебный метод для вывода title объектов
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, verbose_name='Url', unique=True)
+    author = models.CharField(max_length=50)
+    content = models.TextField(blank=True) #необязательное поле
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')# автоматически заполняется текущей датой и не подлежит измению
+    photo = models.ImageField(upload_to='photo/%Y/%M/%D/', blank=True)# место хранения фото
+    viwes =models.IntegerField(default=0, verbose_name='Количество просмотров')# значение по умолчанию =0
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags =models.ManyToManyField(Tag,blank=True, related_name='posts')
+
+    def __str__(self): #служебный метод для вывода title объектов
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
