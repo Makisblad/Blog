9235dd8eee3403483df0697bbdd3e48e680bbaee
Makarova Anastasia <korzhak.an@gmail.com>
2022-09-13 20:02:46 +0300

    The output of posts to the Home page was made

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView,DetailView
+from .models import *
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Посты блога'
+        return context
+
+
 
 def index(request):
     return render(request, 'blog/index.html')
@@ -7,3 +23,5 @@ def index(request):
 def get_category(request, slug):
     return render(request, 'blog/category.html')
 
+def get_post(request, slug):
+    return render(request, 'blog/category.html')
