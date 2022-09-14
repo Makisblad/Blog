dd4be021d29c4702631c14b021d303a96a0da667
Makarova Anastasia <korzhak.an@gmail.com>
2022-09-14 19:48:04 +0300

    Pagination was added

--- siteblog/blog/urls.py
+++ siteblog/blog/urls.py
@@ -5,6 +5,5 @@ from .views import *
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('category/<str:slug>/', get_category, name='category'),
-    path('post/<str:slug>/', get_post, name='post')
-
+    path('post/<str:slug>/', get_post, name='post'),
 ]
--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView, DetailView
 from .models import *
-
-
-class Home(ListView):
-    model = Post
-    template_name = 'blog/index.html'
-    context_object_name = 'posts'
-    paginate_by = 2
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Посты блога'
-        return context
-
-
 
 def index(request):
     return render(request, 'blog/index.html')
@@ -25,3 +11,16 @@ def get_category(request, slug):
 
 def get_post(request, slug):
     return render(request, 'blog/category.html')
+
+class Home (ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'classic blog design'
+        return context
+
+
