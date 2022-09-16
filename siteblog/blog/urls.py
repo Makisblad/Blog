08f06d641862d116b1d87f8a10051f3afa88baf1
Makarova Anastasia <korzhak.an@gmail.com>
2022-09-16 17:12:19 +0300

    Sidebar was made

--- siteblog/blog/urls.py
+++ siteblog/blog/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('category/<str:slug>/', Posts_By_Category.as_view(), name='category'),
     path('post/<str:slug>/', Single_Post.as_view(), name='post'),
-    path('tag/<str:slug>/', Posts_By_Tag.as_view(), name='tag')
+    path('tag/<str:slug>/', Posts_By_Tag.as_view(), name='tag'),
+    path('search/',Search.as_view(),name='search'),
 
 
 ]
