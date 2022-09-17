5174327f3979d25c3966f87df71876f8b24a3bf4
Анастасия Макарова <108862387+Makisblad@users.noreply.github.com>
2022-09-17 11:09:29 +0300

    Update settings.py

--- siteblog/siteblog/settings.py
+++ siteblog/siteblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5%z2*#dclf-tgfq9=&#d#$yh+=9gf@1x1mm20u)g*g8iq(bqv3"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
